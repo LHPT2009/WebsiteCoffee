4ea31f280ef8ee56b2331206eb3fb89f378e40f5
Truong Ton <tonhuyen2000@gmail.com>
2022-11-03 16:30:18 +0700

    Login, register form

--- FrontEnd/src/components/Header/Header.js
+++ FrontEnd/src/components/Header/Header.js
@@ -1,11 +1,10 @@
 import React, { useContext, useEffect } from 'react'
-import logoBugOn from '../../assets/images/logo.png'
 import logo2 from '../../assets/images/logo_2.png'
 import { Link } from 'react-router-dom'
 import { ListProductContext } from '../../context/ListProductContext'
 
 const Header = () => {
-  const { products } = useContext(ListProductContext);
+  const { products } = useContext(ListProductContext)
   return (
     <header className="flex h-[56px] justify-center flex-row items-center bg-s1 leading-5 sticky top-0 left-0 w-full shadow-3 z-[999] font-googleSansRegular">
       <div className="flex flex-row justify-between items-center px-[120px] py-[0px] mx-10  h-[48px] w-full">
@@ -23,16 +22,16 @@ const Header = () => {
                 className="text-black text-center hover:text-primary"
                 to="/product"
               >
-                Homepage
+                Cà phê
               </Link>
             </li>
             <li className="flex items-center px-0 py-[10px] h-[48px] text-center list-none">
-              <a
+              <Link
                 className="text-black text-center hover:text-primary"
-                href=""
+                to="/career"
               >
-                Product
-              </a>
+                Tuyển dụng
+              </Link>
             </li>
             <li className="flex items-center px-0 py-[10px] h-[48px] text-center list-none">
               <Link
@@ -50,23 +49,17 @@ const Header = () => {
               className="text-black text-center hover:text-primary h-[48] w-[48]"
               to="/search"
             >
-              <span class="material-symbols-outlined">
-                shopping_cart
-              </span>
+              <span class="material-symbols-outlined">shopping_cart</span>
             </Link>
           </div>
 
           <div className="flex flex-row justify-between items-center p-[12] m-[12px] h-[48] w-[48]">
-
             <Link
               className="text-black text-center hover:text-primary h-[48] w-[48]"
               to="/search"
             >
-              <span class="material-symbols-outlined">
-                search
-              </span>
+              <span class="material-symbols-outlined">search</span>
             </Link>
-
           </div>
         </div>
       </div>
