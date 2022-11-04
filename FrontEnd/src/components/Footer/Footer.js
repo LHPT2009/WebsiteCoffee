498a4f9a0b20c43784b47b66e243cde0117aebe8
matkhau146 <69472182+matkhau146@users.noreply.github.com>
2022-11-04 21:14:40 +0700

    hoan thien Header Footer

--- FrontEnd/src/components/Footer/Footer.js
+++ FrontEnd/src/components/Footer/Footer.js
@@ -1,53 +1,48 @@
 import React, { useContext, useEffect } from 'react'
 import logoBugOn from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom'
-
+import './Footer.css'
+ 
 const Footer = () => {
 
     return (
-        <footer className="flex flex-row justify-center items-center px-[120px] py-[8] border-t-[1px] border-outline-var bg-s1 w-full">
-            <div className="flex flex-row justify-between items-center px-[20px] w-full">
-                <div className="flex flex-row items-center gap-[24px]">
+        <footer className="flex flex-row justify-center items-center px-[20px] md:px-[120px] py-[8] border-t-[1px] border-outline-var bg-s1 w-full">
+            <div className="flex flex-col lg:flex-row justify-between items-left lg:items-center px-[20px]  w-full">
+                <div className="flex flex-row justify-between items-center gap-[24px]">
                     <img
-                        className=" h-[40px]"
+                        className=" h-[32px]"
                         src={logoBugOn}
                         alt="logo-coffee-bug-on"
                     />
-                    <div className="flex flex-row justify-between items-center">
-                        <div className="flex flex-row justify-between items-center p-[12] m-[12px] h-[48] w-[48]">
+                    <div className="flex flex-row justify-between gap-[32px] items-center mt-[8px]">
+                        <div className="flex flex-row justify-between items-center h-[48] w-[48]">
                             <Link
-                                className="text-black text-center hover:text-primary h-[48] w-[48]"
+                                className="text-primary items-center hover:text-[#548076]"
                                 to="/search"
                             >
-                                <span class="material-symbols-outlined">
-                                    flash_on
-                                </span>
+                                <ion-icon name="logo-facebook"></ion-icon>
                             </Link>
                         </div>
-                        <div className="flex flex-row justify-between items-center p-[12] m-[12px] h-[48] w-[48]">
+                        <div className="flex flex-row justify-between items-center h-[48] w-[48]">
                             <Link
-                                className="text-black text-center hover:text-primary h-[48] w-[48]"
+                                className="text-primary text-center hover:text-[#548076]"
                                 to="/search"
                             >
-                                <span class="material-symbols-outlined">
-                                    flash_on
-                                </span>
+                                <ion-icon name="logo-instagram"></ion-icon>
                             </Link>
                         </div>
-                        <div className="flex flex-row justify-between items-center p-[12] m-[12px] h-[48] w-[48]">
+                        <div className="flex flex-row justify-between items-center h-[48] w-[48]">
                             <Link
-                                className="text-black text-center hover:text-primary h-[48] w-[48]"
+                                className="text-primary text-center hover:text-[#548076] h-[48] w-[48]"
                                 to="/search"
                             >
-                                <span class="material-symbols-outlined">
-                                    flash_on
-                                </span>
+                                <ion-icon name="logo-youtube"></ion-icon>
                             </Link>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className="flex flex-row justify-center items-center p-0 gap-[48px]">
+                    <div className="flex flex-row justify-between items-left md:items-center p-0 gap-[48px]">
                         <li className="flex items-center px-0 py-[10px] h-[48px] text-center text-caption list-none">
                             <Link
                                 className="text-black text-center hover:text-primary"
