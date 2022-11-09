95dd5a7a2008449d412a9242c55a1f0083d429f9
Truong Ton <tonhuyen2000@gmail.com>
2022-11-09 08:01:36 +0700

    Revert

--- FrontEnd/src/components/Header/Header.js
+++ FrontEnd/src/components/Header/Header.js
@@ -8,15 +8,6 @@ import jwtdecode from '../../header/jwt-decode'
 const Header = () => {
   const { products } = useContext(ListProductContext)
   let [open, setOpen] = useState(false)
-  const navigate = useNavigate();
-  const logout = (e) => {
-    e.preventDefault();
-    localStorage.removeItem("token");
-    navigate("/");
-  }
-  const activated = 'active:text-primary'
-  const [active, setActive] = useState('')
-  useEffect(() => {})
   return (
     <header className="flex justify-center items-start bg-s1 sticky top-0 left-0 w-full shadow-3 z-[999] text-l2">
       <div className="flex justify-between items-start px-[20] md:px-[60px] py-[8px] bg-s1 mx-10 w-full">
@@ -39,17 +30,18 @@ const Header = () => {
             </div>
 
             <ul
-              className={`md:flex flex-col md:flex-row absolute md:static md:items-center p-[20px] md:p-0 md:z-auto z-[-100] left-0 w-full md:w-auto gap-[8px] md:gap-[48px] bg-s1 transition-all duration-500 ease-in ${open ? 'top-[56px]' : 'top-[-420px]'
-                } `}
+              className={`md:flex flex-col md:flex-row absolute md:static md:items-center p-[20px] md:p-0 md:z-auto z-[-100] left-0 w-full md:w-auto gap-[8px] md:gap-[48px] bg-s1 transition-all duration-500 ease-in ${
+                open ? 'top-[56px]' : 'top-[-420px]'
+              } `}
             >
-              <button
-                className={`flex px-[16px] items-center text-l2 list-none hover:bg-s5 rounded-[16px] h-[56px] text-black hover:text-primary ${activated}`}
-                onClick={() => {
-                  navigate('/product')
-                }}
-              >
-                Cà phê
-              </button>
+              <li className="flex px-[16px] items-center text-l2 list-none hover:bg-s5 rounded-[16px] h-[56px]">
+                <Link
+                  className="block text-black hover:text-primary w-full"
+                  to="/product"
+                >
+                  Cà phê
+                </Link>
+              </li>
               <li className="flex px-[16px] items-center text-l2 list-none hover:bg-s5 rounded-[16px] h-[56px]">
                 <Link
                   className="text-black text-left hover:text-primary w-full"
@@ -78,7 +70,7 @@ const Header = () => {
               >
                 <span class="material-symbols-outlined">shopping_cart</span>
                 {products.length > 0 && (
-                  <div className="w-[40px] h-[24px] bg-tertiary-cont text-on-tertiary-cont rounded-xl font-medium pt-[2px]">
+                  <div className="w-[40px] h-[24px] bg-tertiary-cont text-on-tertiary-cont rounded-[12px]">
                     {products.length}
                   </div>
                 )}
@@ -99,24 +91,28 @@ const Header = () => {
 
           <div className="hover:bg-s5 rounded-[16px]">
             <div className="flex flex-row justify-between items-center m-[12px]">
-              {/* <Link
+              <Link
                 className="flex flex-row text-black text-center hover:text-primary"
                 to="/signin"
               >
                 <span class="material-symbols-outlined">person</span>
-              </Link> */}
-              {localStorage.getItem("token") ?
+              </Link>
+              {/* {localStorage.getItem('token') ? (
                 <div>
-                  <p>Hello {jwtdecode().name} <button onClick={logout}> Đăng xuất !!!</button></p>
+                  <p>
+                    Hello {jwtdecode().name}{' '}
+                    <button onClick={logout}> Đăng xuất !!!</button>
+                  </p>
                 </div>
-                :
+              ) : (
                 <Link
                   className="flex flex-row text-black text-center hover:text-primary"
                   to="/signin"
                 >
                   <span class="material-symbols-outlined">person</span>
                   <p>Đăng nhập</p>
-                </Link>}
+                </Link>
+              )} */}
             </div>
           </div>
         </div>
