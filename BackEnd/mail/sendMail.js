aff5f4dde02cdd0cf25d25fc54aac97888660a9b
matkhau146 <69472182+matkhau146@users.noreply.github.com>
2022-11-11 21:33:55 +0700

    ChangePasswordUI

--- BackEnd/mail/sendMail.js
+++ BackEnd/mail/sendMail.js
@@ -31,7 +31,69 @@ const Mail = {
         to: `${email}`, // list of receivers
         subject: "Reset Password!", // Subject line
         text: "Reset Password!", // plain text body
-        html: `<h1>${Stext}</h1>`, // html body
+        html: `<body style="margin: 0px; background-color: #F5F5F5; font-size: 16px; border: 1px solid rgba(115, 130, 126, 0.6);">
+        <div style="margin: 40px; color: #1F1F1F;">
+            <div style="display: flex; flex-direction: row; align-items: center; margin-bottom: 24px;">
+                <img src="img/Logo.png" alt="">
+            </div>
+            <div style="width: fit-content;">
+                <h2 style="line-height: 135%;">Thân gửi Khách hàng,</h2>
+                <p style="letter-spacing: 0.04em">Đây là mã thay đổi mật khẩu của tài khoản </p>
+                <div style="display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+                padding: 16px;
+                margin: 48px 0px;
+                border-radius: 16px;
+                color: #07221B;
+                background-color: #dee4e2;">
+                    <h1>${Stext}</h1>
+                </div>
+                <p style="letter-spacing: 0.04em">Email này được gửi vì tài khoản của bạn chọn quên mật khẩu. Bạn có 60s để thay đổi nhập mã.</p>
+                <p>Nếu bạn không phải người thực hiện việc này thì hãy liên hệ cho chúng tôi qua bugOnDev@gmail.com</p>
+                <div style="box-sizing: border-box;
+                display: flex;
+                flex-direction: row;
+                align-items: flex-start;
+                padding: 16px 24px;
+                margin: 56px 0px;
+                border-left: 2px solid #3D685E;">
+                    <p style="letter-spacing: 0.04em">Trân thành cảm ơn,<br>
+                        Bug Ổn Team</p>
+                </div>
+                <div style="height: 0px;
+                border: 1px solid rgba(115, 130, 126, 0.6);
+                margin: 16px 0px;">
+                </div>
+                <div class="footer">
+                    <ul style="display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    align-items: center;
+                    padding-top: px;
+                    gap: 24px;
+                    list-style: none;">
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/img/Facebook.png">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/img/Instagram.png">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <img src="/img/Youtube.png">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>`, // html body
       }, (err) => {
         if (err) {
           return res.json({
--- FrontEnd/src/components/Search/Search.js
+++ FrontEnd/src/components/Search/Search.js
@@ -1,26 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
 import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, Hits  } from 'react-instantsearch-hooks-web';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 const searchClient = algoliasearch('00SC4J6Z9T', 'd0343528014a311175465c9e87cd21fc');
 
-function Hit({ hit }) {
-    return (
-      <article>
-        <img src={hit.image} alt={hit.name} />
-        <h1>{hit.name}</h1>
-        <p>${hit.price}</p>
-      </article>
-    );
-  }
-
-function Search() {
-  return (
-    <InstantSearch searchClient={searchClient} indexName="product_search">
-      <SearchBox />
-    </InstantSearch>
-  )
-}
-
+const Search = () => (
+  <InstantSearch searchClient={searchClient} indexName="demo_ecommerce">
+    <SearchBox />
+    <Hits />
+  </InstantSearch>
+);
 export default Search
