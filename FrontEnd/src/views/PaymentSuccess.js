72d35fdea4819a9884be0b2a9da4ded3ed67476c
lehuynhphuongtung <lehuynhphuongtung02@gmail.com>
2022-11-16 18:02:57 +0700

    MoMo

--- FrontEnd/src/views/PaymentSuccess.js
+++ FrontEnd/src/views/PaymentSuccess.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useEffect, params, useState } from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import axios from 'axios';
+import jwt_decode from 'jwt-decode'
+import { useLocation } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
 const PaymentSuccess = () => {
+  const navigate = useNavigate();
+  const search = useLocation().search;
+  const message = new URLSearchParams(search).get('message');
+
+  const products = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+  const userid = localStorage.getItem('token') ? jwt_decode(localStorage.getItem('token')).id : "";
+  const price = new URLSearchParams(search).get('amount');
+  const statuspayment = true;
+  const statusdelivery = false;
+  const discountid = localStorage.getItem('discount') ? JSON.parse(localStorage.getItem('discount'))._id : "";
+  const discountprice = localStorage.getItem('discount') ? JSON.parse(localStorage.getItem('discount')).price : 0;
+
+  if (message == "Successful.") {
+    if (localStorage.getItem('cart') && localStorage.getItem('discount')) {
+      const rec = axios.post('http://localhost:8000/receipt', {
+        userid,
+        price,
+        products,
+        discountid,
+        discountprice,
+        statuspayment,
+        statusdelivery,
+      })
+      console.log("thu danh dau", message);
+      localStorage.removeItem('cart');
+      localStorage.removeItem('discount');
+      navigate("/paymentsuccess");
+    }
+  }
   return (
     <div className="relative min-h-screen pb-24 lg:pb-12">
       <Header />
       <div className="mt-10 mx-[-15px] sm:mx-5 md:mx-[50px] lg:mx-[100px] xl:mx-[150px]">
-        <div className="text-center text-[100px] font-bold">THÀNH CÔNG</div>
+        <div className="text-center text-[100px] font-bold">Thanh toán thành công!!!</div>
       </div>
       <Footer />
     </div>
