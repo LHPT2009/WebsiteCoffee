fcea0dc2ef856a16c3284d3a5b38d0c0221c8563
matkhau146 <69472182+matkhau146@users.noreply.github.com>
2022-11-16 18:13:02 +0700

    update ui cart

--- FrontEnd/src/components/Input/TextInput.js
+++ FrontEnd/src/components/Input/TextInput.js
@@ -13,7 +13,7 @@ const TextInput = ({
 }) => {
   return (
     <input
-      className={`${className}  border-outline-var border-style: solid rounded-full text-l2 mb-[16px] pt-[13px] px-[16px] pb-[13px] 
+      className={`${className}  border-outline-var border-[2px] rounded-full text-l2 mb-[16px] pt-[13px] px-[16px] pb-[13px] 
       hover:border-primary hover:rounded-[32px] focus:border-primary focus:rounded-[16px] focus:text-on-primary-cont focus:bg-primary-cont transition-all ease-in duration-300`}
       onChange={onChange}
       type={type}
