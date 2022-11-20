f428cf6cc6a026a7f990229d406566f03dbc06fa
Truong Ton <tonhuyen2000@gmail.com>
2022-11-20 12:58:10 +0700

    Edit profile

--- FrontEnd/src/views/Profile.js
+++ FrontEnd/src/views/Profile.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import jwt_decode from 'jwt-decode'
-
+import axios from 'axios'
 import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import TextInput from '../components/Input/TextInput'
 import Button from '../components/Button/Button'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Profile = () => {
   const navigate = useNavigate()
@@ -14,6 +14,44 @@ const Profile = () => {
     localStorage.removeItem('token')
     navigate('/')
   }
+
+  const [currUser, setCurrUser] = useState([])
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:8000/user/${
+          jwt_decode(localStorage.getItem('token')).id
+        }`
+      )
+      .then((res) => {
+        setCurrUser(res.data)
+      })
+  }, [])
+  const [email, setEmail] = useState(currUser.email)
+  const [firstname, setFirstname] = useState(currUser.firstname)
+  const [lastname, setLastname] = useState(currUser.lastname)
+  const [numberphone, setNumberphone] = useState(currUser.numberphone)
+
+  const editUser = async (e) => {
+    e.preventDefault()
+    const edit = await axios.put(
+      `http://localhost:8000/user/${
+        jwt_decode(localStorage.getItem('token')).id
+      }`,
+      {
+        email,
+        firstname,
+        lastname,
+        numberphone,
+      }
+    )
+    if (edit) {
+      navigate('/profile')
+    } else {
+      alert('Sua ko thanh cong!!!')
+    }
+  }
+
   return (
     <div className="relative min-h-screen pb-24 lg:pb-12">
       <Header />
@@ -26,48 +64,75 @@ const Profile = () => {
           <div className="w-[80%] box-border pl-5">
             <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
               <TextInput
-                className="flex-1 mb-0 hover:border-outline-var"
+                className="flex-1 !mb-0 hover:border-outline-var"
                 disabled={'disabled'}
-                defaultValue={jwt_decode(localStorage.getItem('token')).name}
+                defaultValue={currUser.username}
               />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-end mt-3">
+        <div className="flex items-center justify-end mt-4">
           <div className="w-[20%] text-left overflow-hidden">Email</div>
           <div className="w-[80%] box-border pl-5">
             <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
               <TextInput
-                className="flex-1 mb-0"
-                defaultValue={jwt_decode(localStorage.getItem('token')).name}
+                className="flex-1 !mb-0"
+                onChange={(e) => setEmail(e.target.value)}
+                defaultValue={currUser.email}
               />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-end mt-3">
+        <div className="flex items-center justify-end mt-4">
+          <div className="w-[20%] text-left overflow-hidden">Họ</div>
+          <div className="w-[80%] box-border pl-5">
+            <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
+              <TextInput
+                className="flex-1 !mb-0"
+                onChange={(e) => setLastname(e.target.value)}
+                defaultValue={currUser.lastname}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end mt-4">
+          <div className="w-[20%] text-left overflow-hidden">Tên</div>
+          <div className="w-[80%] box-border pl-5">
+            <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
+              <TextInput
+                className="flex-1 !mb-0"
+                onChange={(e) => setFirstname(e.target.value)}
+                defaultValue={currUser.firstname}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end mt-4">
           <div className="w-[20%] text-left overflow-hidden">Số điện thoại</div>
           <div className="w-[80%] box-border pl-5">
             <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
               <TextInput
-                className="flex-1 mb-0"
-                defaultValue={jwt_decode(localStorage.getItem('token')).name}
+                className="flex-1 !mb-0"
+                onChange={(e) => setNumberphone(e.target.value)}
+                defaultValue={currUser.numberphone}
               />
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-end mt-3">
+        <div className="flex items-center justify-end mt-4">
           <div className="w-[20%] text-left overflow-hidden">Địa chỉ</div>
           <div className="w-[80%] box-border pl-5">
             <div className="flex items-center box-border w-full h-[40%] overflow-hidden">
               <TextInput
-                className="flex-1 mb-0"
-                defaultValue={jwt_decode(localStorage.getItem('token')).name}
+                className="flex-1 !mb-0"
+                onChange={''}
+                defaultValue={''}
               />
             </div>
           </div>
         </div>
         <div className="flex justify-end mt-5">
-          <Button icon="" onClick={''}>
+          <Button icon="" onClick={editUser}>
             Lưu
           </Button>
         </div>
